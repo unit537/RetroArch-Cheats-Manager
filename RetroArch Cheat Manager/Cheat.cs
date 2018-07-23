d15652fbca276268464e2b4bd03612c0a13ef566
Cody Greene <unit537gaming@gmail.com>
2018-07-23 00:27:04 -0400

    Initial Project Commit

---
+++ RetroArch Cheat Manager/Cheat.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RetroArch_Cheat_Manager
+{
+    class Cheat
+    {
+        private string name;    // This is the name of the Cheat as it will appear in RetroArch
+        private string code;    // List of codes used by this cheat, taken directly from user input (Not Always Correct!)
+        private bool enable;    // This is the status of the cheat when the file is loaded (Enabled/Disabled)
+        private int id;         // Unique id used to order Code in the cheat file
+
+        public string Name { get => name; set => name = value; }
+        public string Code { get => code; set => code = value; }
+        public bool Enable { get => enable; set => enable = value; }
+        public int Id { get => id; set => id = value; }
+        
+        public Cheat(string name, string code, bool enable, int id)
+        {
+            this.name = name;
+            this.code = code;
+            this.enable = enable;
+            this.id = id;
+        }
+
+        public Cheat(string name, string code, bool enable)
+        {
+            this.name = name;
+            this.code = code;
+            this.enable = enable;
+            this.id = -1;
+        }
+        
+        public Cheat()
+        {
+            this.name = "null";
+            this.code = "null";
+            this.enable = false;
+            this.id = -1;
+        }
+        
+        /*
+         * The proper ToString override, returns a string representing a single cheat entry
+         * for the cheat file used by RetroArch. Because the user inputted code(s) may contain
+         * garbage, we attempt to remove everything but the code itself, and replace every space
+         * with a '+' character. Each code ends with a '+' for convenience, at this time this
+         * does not appear to cause any issues with RetroArch.
+         * 
+         * TODO: Investigate ending Code with a '+' char to determine potential issues.
+         */
+        public override string ToString()
+        {
+            StringBuilder cheat = new StringBuilder();
+            
+            code = code.Replace("\r\n", " ");
+            code = code.Replace("\n\r", " ");
+            code = code.Replace("\r", " ");
+            code = code.Replace("\n", " ");
+            code = code.Trim().Replace(" ", "+");
+
+            cheat.Append("cheat" + this.id + "_desc = \"" + this.name + "\"\n");
+            cheat.Append("cheat" + this.id + "_code = \"" + this.code + "\"\n");
+            cheat.Append("cheat" + this.id + "_enable = " + this.enable + "\n\n");
+
+            return cheat.ToString();
+        }
+
+        /*
+         * So, I didn't know that you could combine multiple Code into one line
+         * by simply inserting a '+' between every space and newline...
+         * So don't do this, but I already did it, so it's still here.
+         * 
+         * Note: If you do this, you'll need to track 'id+Code.length' as your
+         * actual cheat id for each code in the file. This means you need to be very
+         * careful with assigning ids initially. To resolve this issue, always assign
+         * a new id as the previous cheat's 'id+Code.length+1'.
+         */
+        public string ToStringOld()
+        {
+            StringBuilder cheat = new StringBuilder();
+
+            for (int i = 0; i < this.Code.Length; i++)
+            {
+                cheat.Append("cheat" + (this.id + i) + "_desc = \"" + this.name + "\"\n");
+                cheat.Append("cheat" + (this.id + i) + "_code = \"" + this.Code[i] + "\"\n");
+                cheat.Append("cheat" + (this.id + i) + "_enable = " + this.enable + "\n\n");
+            }
+            return cheat.ToString();
+        }
+    }
+}
---
+++ RetroArch Cheat Manager/MainWindow.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace RetroArch_Cheat_Manager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int cheatCount = 0;
+        int currentCheatID = -1;
+        String filepath;
+        String fileContents;
+        List<Cheat> cheats;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cheats = new List<Cheat>();
+            UpdateCheatCount(cheatCount);
+        }
+
+        private void UpdateCheatCount(int n)
+        {
+            cheatCount = n;
+            label_CurrentCheats.Content = "Current Cheats (" + cheatCount + "):";
+            PopulateListBox();
+        }
+
+        private void PopulateListBox()
+        {
+            listbox_CheatsList.Items.Clear();
+            for (int i = 0; i < cheatCount; i++)
+            {
+                cheats.ElementAt(i).Id = i;
+                listbox_CheatsList.Items.Add(cheats.ElementAt(i).Name);
+            }
+        }
+
+        private void NewFile(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "RetroArch Cheat Files (*.cht)|*.cht";
+            sfd.Title = "Save a New RetroArch Cheat File";
+            if (sfd.ShowDialog() == true)
+            {
+                filepath = sfd.FileName;
+            }
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            cheats = new List<Cheat>();
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "RetroArch Cheat Files (*.cht)|*.cht";
+            ofd.Title = "Open a RetroArch Cheat File";
+
+            if (ofd.ShowDialog() == true)
+            {
+                filepath = ofd.FileName;
+                fileContents = File.ReadAllText(filepath);
+            }
+
+            string[] lines = fileContents.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                if (line.Contains("cheats = "))
+                {
+                    line = line.Remove(0, 8);
+                    line.Trim();
+                    cheatCount = Int32.Parse(line);
+
+                    for (int j = 0; j < cheatCount; j++)
+                    {
+                        cheats.Add(new Cheat());
+                        //cheats.Insert(i, new Cheat());
+                    }
+                    UpdateCheatCount(cheatCount);
+                }
+
+                if (line.Contains("_desc"))
+                {
+                    line = line.Remove(0, 5);
+                    int id = Int32.Parse( line.Remove( line.IndexOf("_") ) );
+                    string name = line.Substring(line.IndexOf("\"") + 1, line.Length - line.IndexOf("\"") - 2);
+                    cheats.ElementAt(id).Name = name;
+                    //cheats.ElementAt(id].Id = id;
+                }
+
+                if (line.Contains("_code"))
+                {
+                    line = line.Remove(0, 5);
+                    int id = Int32.Parse(line.Remove(line.IndexOf("_")));
+                    string code = line.Substring(line.IndexOf("\"") + 1, line.Length - line.IndexOf("\"") - 2);
+                    cheats.ElementAt(id).Code = code;
+                }
+
+                if (line.Contains("_enable"))
+                {
+                    line = line.Remove(0, 5);
+                    int id = Int32.Parse(line.Remove(line.IndexOf("_")));
+                    string enable = line.Substring(line.IndexOf("=") + 2).ToLower();
+                    if (enable == "false")
+                    {
+                        cheats.ElementAt(id).Enable = false;
+                    }
+                    else if (enable == "true")
+                    {
+                        cheats.ElementAt(id).Enable = true;
+                    }
+                }
+            }
+            PopulateListBox();
+        }
+
+        private void SaveFile(object sender, RoutedEventArgs e)
+        {
+            StringBuilder cheatFileContents = new StringBuilder();
+
+            cheatFileContents.Append("cheats = " + cheatCount + "\n\n");
+
+            for (int i = 0; i < cheats.Count; i++)
+            {
+                cheatFileContents.Append(cheats.ElementAt(i).ToString());
+            }
+
+            File.WriteAllText(filepath, cheatFileContents.ToString());
+        }
+
+        private void DeleteCheat(object sender, RoutedEventArgs e)
+        {
+            int index = listbox_CheatsList.SelectedIndex;
+            if (index != -1)
+            {
+                cheats.RemoveAt(index);
+                cheatCount--;
+                UpdateCheatCount(cheatCount);
+            }
+        }
+
+        private void EditCheat(object sender, RoutedEventArgs e)
+        {
+            int listBoxIndex = listbox_CheatsList.SelectedIndex;
+            if (listBoxIndex != -1)
+            {
+                textbox_CheatName.Text = cheats.ElementAt(listBoxIndex).Name;
+                textbox_CheatCodes.Text = cheats.ElementAt(listBoxIndex).Code;
+                currentCheatID = listBoxIndex;
+            }
+        }
+
+        private void SaveCheat(object sender, RoutedEventArgs e)
+        {
+            if (currentCheatID != -1)
+            {
+                cheats.ElementAt(currentCheatID).Name = textbox_CheatName.Text;
+                cheats.ElementAt(currentCheatID).Code = textbox_CheatCodes.Text;
+                PopulateListBox();
+            }
+            else if (currentCheatID == -1)
+            {
+                AddCheat(sender, e);
+            }
+        }
+
+        private void AddCheat(object sender, RoutedEventArgs e)
+        {
+            string name = textbox_CheatName.Text;
+            string code = textbox_CheatCodes.Text;
+            cheats.Add(new Cheat(name, code, false, cheatCount));
+            cheatCount++;
+            UpdateCheatCount(cheatCount);
+        }
+    }
+}
